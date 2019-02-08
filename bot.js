96b5c4282fc908699123217728ee2747851b645a
alialmeahy <45081465+alialmeahy@users.noreply.github.com>
2019-02-08 04:23:55 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,39 @@
-﻿const Discord = require("discord.js");
-const RichEmbed = require("discord.js");
-const { Client, Util } = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "$";
-
 const devs = ["435182410531864577"]
 
-const adminprefix = "-";
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+const adminprefix = "$";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
